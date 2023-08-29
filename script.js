b72591d519b344595f2e7fcff8592de2901768fa
Brian Miller <143358182+BelowAverageGamer@users.noreply.github.com>
2023-08-28 22:28:34 -0400

    Chase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pandas = ["bao-bao.html", "mei-xiang.html"];
 
 function randomPanda(){
-    location.href = (Math.floor((Math.random() * this.pandas.length())))
+    location.href = pandas[(Math.floor((Math.random() * this.pandas.length())))]
 };
 
